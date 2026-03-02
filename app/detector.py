b6109c6ff45288b9e0b0c046340b65dfa034c60f
Tatiana Delgadillo <tai.delgadillo@gmail.com>
2026-03-02 01:13:18 -0400

    reduce models to ocr

--- app/detector.py
+++ app/detector.py
@@ -69,7 +69,7 @@ _reader = None
 def get_reader():
     global _reader
     if _reader is None:
-        _reader = easyocr.Reader(["es", "en"], gpu=False)
+        _reader = easyocr.Reader(["en"], gpu=False)
     return _reader
 
 
